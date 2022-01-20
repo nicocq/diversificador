35445311a471e160e73644470d0183501950b8fc
nicoc <nicolas.calfuquir@sesocio.com>
2022-01-20 17:07:21 -0300

    Estructura basica con html y js.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.getElementById("btn-ingresos").addEventListener('click', onClickIngresos); 
+document.getElementById("btn-configuracion").addEventListener('click', onClickConfiguracion); 
+
+function onClickIngresos(){
+    alert("Funcionalidad de ingresos");
+};
+function onClickConfiguracion(){
+    alert("Funcionalidad de configuracion");
+}
