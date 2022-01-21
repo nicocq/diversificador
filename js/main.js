4994ea0ceae2a726084004e1ba069842422ad830
nicoc <nicolas.calfuquir@sesocio.com>
2022-01-21 12:36:45 -0300

    Creando objetos basicos para maniupar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 document.getElementById("btn-ingresos").addEventListener('click', onClickIngresos); 
 document.getElementById("btn-configuracion").addEventListener('click', onClickConfiguracion); 
 
@@ -7,3 +8,53 @@ function onClickIngresos(){
 function onClickConfiguracion(){
     alert("Funcionalidad de configuracion");
 }
+
+// CLASES
+class ConfiguracionActivo {
+    constructor(activo, porcentaje){
+        this.activo = activo;
+        this.porcentaje = porcentaje;
+    }
+
+    toString() { return `[ConfiguracionActivo]
+        activo:       ${this.activo},
+        porcentaje:   ${this.porcentaje}
+    `;
+    };
+}
+
+class Activo {
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+}
+
+class Diversificador {
+    constructor(configuracion){
+        this.configuracion = configuracion;
+    }
+
+    porcentaje(numero, porcentaje){
+        /** TODO */
+    }
+
+    diversificar(ingresos){ 
+        /** TODO */
+    }
+}
+
+class Patron {
+    constructor(){
+        this.configuracion_activos = [];
+    }
+}
+
+function main(){
+    console.log("Iniciando diversificador");
+    let configuracion = new ConfiguracionActivo("BTC", 123);
+    console.log(configuracion);
+    console.log(configuracion.toString());
+}
+
+
+main();
