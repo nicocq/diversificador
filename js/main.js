44fb043ae433686bcb9a8481e737ea800d25867d
nicoc <nicolas.calfuquir@sesocio.com>
2022-01-26 11:11:09 -0300

    Valido que activo y porcentaje no sean vacios. Boton Agregar

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 
-document.getElementById("btn-diversificar").addEventListener('click', onClickDiversificar); 
-document.getElementById("btn-configuracion").addEventListener('click', onClickConfiguracion); 
-document.getElementById("btn-cancelar").addEventListener('submit', onClickCancelarConfiguracion)
+document.getElementById("btn-diversificar").addEventListener('click', onClickDiversificar);
+document.getElementById("btn-configuracion").addEventListener('click', onClickConfiguracion);
+document.getElementById("btn-cancelar").addEventListener('click', onClickCancelarConfiguracion)
 document.getElementById("form-configuracion").addEventListener('submit', onClickGuardarConfiguracion)
 
-function log(mensaje){
+function log(mensaje) {
     let log_element = document.getElementById('mensajes-log');
     log_element.innerHTML = '';
-    log_element.innerHTML = mensaje; 
+    log_element.innerHTML = mensaje;
 }
 
-function onClickCancelarConfiguracion(){
+function onClickCancelarConfiguracion(e) {
+    e.preventDefault();
     document.getElementById('form-configuracion-container').style.display = "none";
 }
 
-function onClickGuardarConfiguracion(e){
+function onClickGuardarConfiguracion(e) {
     e.preventDefault();
 
     let form = document.getElementById("form-configuracion");
@@ -22,11 +23,11 @@ function onClickGuardarConfiguracion(e){
     let porcentaje_inputs = form.querySelectorAll('[name*=porcentaje]');
 
     let objeto = {}
-    for (let i =0; i < activo_inputs.length; i++ ){
+    for (let i = 0; i < activo_inputs.length; i++) {
         objeto[activo_inputs[i].value] = porcentaje_inputs[i].value;
     }
-    patron = Patron.create_from_object({activos: objeto});
-    if (!patron.is_valid() ) {
+    patron = Patron.create_from_object({ activos: objeto });
+    if (!patron.is_valid()) {
         log(patron.errores);
         return false;
     }
@@ -40,19 +41,20 @@ function onClickGuardarConfiguracion(e){
     return false;
 }
 
-function onClickDiversificar(){
+function onClickDiversificar() {
     let ingresos = document.getElementById("input-ingresos").value;
     ingresos = parseFloat(ingresos) || 0;
 
     let almacenamiento = new Almacenamiento();
     let patron_data = almacenamiento.get_patron();
     let patron = Patron.create_from_object(patron_data);
-    
+
     let diversificador = new Diversificador(patron);
     let resultado = diversificador.diversificar(ingresos);
     document.getElementById("resultado-json").textContent = JSON.stringify(resultado, undefined, 2);
 };
-function onClickConfiguracion(){
+
+function onClickConfiguracion() {
     console.log("Funcionalidad de configuracion");
     document.getElementById('form-configuracion-container').style.display = "block";
     let almacenamiento = new Almacenamiento();
@@ -60,14 +62,14 @@ function onClickConfiguracion(){
     let patron = Patron.create_from_object(patron_data);
     let table_body_contaier = document.getElementById('table-container');
     table_body_contaier.innerHTML = '';
-    let ids= 0;
+    let ids = 0;
     for (let [activo, porcentaje] of Object.entries(patron.get_activos())) {
         tr = document.createElement('tr');
-        
+
         td = document.createElement('td');
         input = document.createElement('input');
         label = document.createElement('label');
-        input.name = 'activo_' + ids; 
+        input.name = 'activo_' + ids;
         input.value = activo;
         td.appendChild(input);
         tr.appendChild(td);
@@ -75,20 +77,49 @@ function onClickConfiguracion(){
         td = document.createElement('td');
         input = document.createElement('input');
         label = document.createElement('label');
-        input.name = 'porcentaje_' + ids; 
+        input.name = 'porcentaje_' + ids;
         input.value = porcentaje;
         td.appendChild(input);
-        tr.appendChild(td);       
-       
+        tr.appendChild(td);
+
         table_body_contaier.appendChild(tr);
         ids += 1;
     }
-    
+    let button_add = document.createElement('button');
+    button_add.innerHTML = "Agregar"
+    button_add.addEventListener('click', (e) => {
+        e.preventDefault();
+        create_empty_option(table_body_contaier);
+    });
+    table_body_contaier.appendChild(button_add);
+}
+
+function create_empty_option(table_body_contaier){
+    tr = document.createElement('tr');
+    td = document.createElement('td');
+    input = document.createElement('input');
+    label = document.createElement('label');
+    input.name = 'activo_0';
+    input.placeholder = "Nuevo activo";
+    td.appendChild(input);
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    input = document.createElement('input');
+    label = document.createElement('label');
+    input.name = 'porcentaje_0';
+    input.placeholder = "Nuevo porcentaje";
+    td.appendChild(input);
+    tr.appendChild(td);
+
+    let last_tr = table_body_contaier.getElementsByTagName('tr');
+    last_tr = last_tr[last_tr.length - 1];
+    table_body_contaier.insertBefore(tr, last_tr.nextSibling );
 }
 
 // CLASES
 class Diversificador {
-    constructor(patron){
+    constructor(patron) {
         this.patron = patron;
     }
 
@@ -98,23 +129,24 @@ class Diversificador {
         return {
             ingresos_totales: ingresos,
             resultado: Object.entries(this.patron.get_activos())
-            .map(([activo, porcentaje]) => { 
-                return {
-                    activo: activo,
-                    porcentaje: porcentaje, 
-                    monto: this.porcentaje(ingresos, porcentaje)} 
-            }) 
-        };        
+                .map(([activo, porcentaje]) => {
+                    return {
+                        activo: activo,
+                        porcentaje: porcentaje,
+                        monto: this.porcentaje(ingresos, porcentaje)
+                    }
+                })
+        };
     }
 }
 
 class Patron {
-    constructor(){
+    constructor() {
         this.activos = {};
         this.errores = [];
     }
 
-    static create_from_object(data){
+    static create_from_object(data) {
         let patron = new Patron();
         if (data) {
             patron.activos = data.activos;
@@ -123,20 +155,28 @@ class Patron {
     }
 
     add_activo = (nombre, porcentaje) => this.activos[nombre] = porcentaje;
-    
-    get_activos = () => this.activos ;
+
+    get_activos = () => this.activos;
 
     es_vacio = () => Object.entries(this.activos).length === 0;
 
     is_valid = () => {
-        let total = Object.values(this.get_activos()).reduce((e1, e2) => {
-            return parseFloat(e1) + parseFloat(e2);
-        });
+        let total = 0;
+        Object.entries(this.get_activos()).forEach(([activo, porcentaje]) => {
+            if (! activo) {
+                this.errores.push("Campo Activo no puede ser vacio");    
+            }
+            if (! porcentaje) {
+                this.errores.push("Campo Porcentaje no puede ser vacio");    
+            }
+            total += parseFloat(porcentaje);
+        })
 
-        if (total > 100){
+        console.log(total);
+        if (total > 100) {
             this.errores.push("El total de porcentajes no debe superar el 100%");
         }
-        if (total < 1){
+        if (total < 1) {
             this.errores.push("El total debe ser mayor a 0");
         }
 
@@ -145,27 +185,27 @@ class Patron {
 }
 
 class Almacenamiento {
-    constructor(){}
+    constructor() { }
 
-    guardar = (key, value) => localStorage.setItem(key, value);  
+    guardar = (key, value) => localStorage.setItem(key, value);
 
-    recuperar = (key) => localStorage.getItem(key); 
+    recuperar = (key) => localStorage.getItem(key);
 
     eliminar = (key) => localStorage.removeItem(key);
 
-    get_patron = () => JSON.parse(this.recuperar('patron')); 
+    get_patron = () => JSON.parse(this.recuperar('patron'));
 
     set_patron = (data) => this.guardar('patron', JSON.stringify(data));
-}   
+}
 
-function main(){
+function main() {
     console.log("Iniciando diversificador");
     let almacenamiento = new Almacenamiento();
     document.getElementById('form-configuracion-container').style.display = "none";
-
+    
     let patron_data = almacenamiento.get_patron();
     let patron = Patron.create_from_object(patron_data);
-    if ( patron.es_vacio()){ 
+    if (patron.es_vacio()) {
         patron = new Patron();
         patron.add_activo('BTC', 10);
         patron.add_activo('ALT', 10);
